bd4cc7d51883e291c48d23a175c3ecb35495fabc
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2022-07-14 04:31:21 +0200

    X7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Nav from "./components/Nav";
 import Home from "./pades/Home";
+import Layout from '../src/components/Layout'
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Home/>
+      <Layout />
     </div>
   );
 }

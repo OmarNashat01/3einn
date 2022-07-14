5d612e7948c7e8c1343b027f20de86055e3a21ae
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2022-07-14 04:38:04 +0200

    X9

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import ArrowRightAltIcon from '@mui/icons-material/ArrowForward';
+import { Link } from 'react-router-dom';
 const Layout = () => {
   return (
-    <div>
+    <div >
+       <div >
+                <Link to='/products'>
+                <p>shop now </p>
+                <ArrowRightAltIcon fontSize="large"/>
+                </Link> 
+       </div>
+        
     </div>
   )
 }

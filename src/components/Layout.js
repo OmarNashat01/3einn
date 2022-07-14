8ef28a29f94780345158648278461a74b423277b
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2022-07-14 04:40:11 +0200

    X11

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import classes from './style/layout.module.css'
+import classes from './style/Layout.module.css'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowForward';
 import { Link } from 'react-router-dom';
 const Layout = () => {

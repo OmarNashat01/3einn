48ce960275088c7f7d7cd80421202f0b578fde4a
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2022-07-14 04:09:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-
+import Home from "./pades/Home";
+import {Switch, Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Switch>
+      <Route path="/" exact>
+      <Home/>
+      </Route>
+      </Switch>
+     <Footer/>
     </div>
   );
 }

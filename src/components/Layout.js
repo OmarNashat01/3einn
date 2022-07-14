4ffcde4289bae3bce4ebbeeab6c593a03313b848
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2022-07-14 05:28:20 +0200

    F12

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from './style/layout.module.css'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowForward';
 import { Link } from 'react-router-dom';
+
 const Layout = () => {
   return (
     <div className={classes.layout}>

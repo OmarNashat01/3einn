e14f2b45860c838e97c2e99084ef4928c1edefb0
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2022-07-14 04:39:06 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import classes from './style/layout.module.css'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowForward';
 import { Link } from 'react-router-dom';
 const Layout = () => {
   return (
-    <div >
-       <div >
-                <Link to='/products'>
+    <div className={classes.layout}>
+       <div className={classes.textBox}>
+                <Link to='/products' className={classes.btn} >
                 <p>shop now </p>
-                <ArrowRightAltIcon fontSize="large"/>
+                <ArrowRightAltIcon fontSize="large"  className={classes.arrow}/>
                 </Link> 
        </div>
         

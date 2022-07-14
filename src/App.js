68521b544e1b031d056f235a41507d081bd4dea0
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2022-07-14 04:32:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import Nav from "./components/Nav";
-import Home from "./pades/Home";
+
 import Layout from '../src/components/Layout'
 function App() {
   return (
     <div className="App">
       <Nav/>
-      <Home/>
+      
       <Layout />
     </div>
   );

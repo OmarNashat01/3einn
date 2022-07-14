671a0008ef7a358fdc4b68b95943880dd2318010
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2022-07-14 04:28:43 +0200

    X5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import Footer from "./components/Footer";
+
 import Nav from "./components/Nav";
 import Home from "./pades/Home";
-import {Switch, Route} from "react-router-dom"
-import Allproducts from "./pades/Allproducts";
-import ProductDetailes from "./pades/ProductDetails";
-import Cart from "./pades/Cart";
 function App() {
   return (
     <div className="App">
